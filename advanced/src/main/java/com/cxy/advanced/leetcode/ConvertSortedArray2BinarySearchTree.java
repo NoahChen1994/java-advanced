d366d8ebe1f87971ff38cf2029881a447566aa86
chenxinyi <xinyi.chen@karakal.com.cn>
2023-02-15 16:11:46 +0800

    有序数组转换为平衡二叉树

---
+++ advanced/src/main/java/com/cxy/advanced/leetcode/ConvertSortedArray2BinarySearchTree.java
@@ -0,0 +1,120 @@
+package com.cxy.advanced.leetcode;
+
+/**
+ * @author xinyi.chen
+ * @date 2022/10/12 0012
+ * @description 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ *                  高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1」的二叉树。
+ * @since JDK1.8
+ */
+public class ConvertSortedArray2BinarySearchTree {
+
+    public static void main(String[] args) {
+        TreeNode solution = solution(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11});
+        show(solution);
+    }
+
+    private static TreeNode solution(int[] num){
+        return helper(num, 0, num.length-1);
+    }
+
+    private static TreeNode helper(int[] num, int left, int right) {
+        if (left>right){
+            return null;
+        }
+        int mid = (left+right)>>1;
+        TreeNode root = new TreeNode(num[mid]);
+        root.left = helper(num, left, mid-1);
+        root.right = helper(num, mid+1, right);
+        return root;
+    }
+
+    //Definition for a binary tree node.
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    // 用于获得树的层数
+    private static int getTreeDepth(TreeNode root) {
+        return root == null ? 0 : (1 + Math.max(getTreeDepth(root.left), getTreeDepth(root.right)));
+    }
+
+    private static void writeArray(TreeNode currNode, int rowIndex, int columnIndex, String[][] res, int treeDepth) {
+        // 保证输入的树不为空
+        if (currNode == null){
+            return;
+        }
+        // 先将当前节点保存到二维数组中
+        res[rowIndex][columnIndex] = String.valueOf(currNode.val);
+
+        // 计算当前位于树的第几层
+        int currLevel = ((rowIndex + 1) / 2);
+        // 若到了最后一层，则返回
+        if (currLevel == treeDepth) {
+            return;
+        }
+        // 计算当前行到下一行，每个元素之间的间隔（下一行的列索引与当前元素的列索引之间的间隔）
+        int gap = treeDepth - currLevel - 1;
+
+        // 对左子节点进行判断，若存在左字节点，则记录相应的"/"与左儿子的值
+        if (currNode.left != null) {
+            res[rowIndex + 1][columnIndex - gap] = "/";
+            writeArray(currNode.left, rowIndex + 2, columnIndex - gap * 2, res, treeDepth);
+        }
+
+        // 对右子节点进行判断，若有右子节点，则记录相应的"\"与右儿子的值
+        if (currNode.right != null) {
+            res[rowIndex + 1][columnIndex + gap] = "\\";
+            writeArray(currNode.right, rowIndex + 2, columnIndex + gap * 2, res, treeDepth);
+        }
+    }
+
+
+    public static void show(TreeNode root) {
+        if (root == null){
+            System.out.println("EMPTY!");
+            return;
+        }
+        // 得到树的深度
+        int treeDepth = getTreeDepth(root);
+
+        // 最后一行的宽度为2的（n - 1）次方乘3，再加1
+        // 作为整个二维数组的宽度
+        int arrayHeight = treeDepth * 2 - 1;
+        int arrayWidth = (2 << (treeDepth - 2)) * 3 + 1;
+        // 用一个字符串数组来存储每个位置应显示的元素
+        String[][] res = new String[arrayHeight][arrayWidth];
+        // 对数组进行初始化，默认为一个空格
+        for (int i = 0; i < arrayHeight; i++) {
+            for (int j = 0; j < arrayWidth; j++) {
+                res[i][j] = " ";
+            }
+        }
+
+        // 从根节点开始，递归处理整个树
+        // res[0][(arrayWidth + 1)/ 2] = (char)(root.val + '0');
+        writeArray(root, 0, arrayWidth / 2, res, treeDepth);
+
+        // 此时，已经将所有需要显示的元素储存到了二维数组中，将其拼接并打印即可
+        for (String[] line : res) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < line.length; i++) {
+                sb.append(line[i]);
+                if (line[i].length() > 1 && i <= line.length - 1) {
+                    i += line[i].length() > 4 ? 2 : line[i].length() - 1;
+                }
+            }
+            System.out.println(sb);
+        }
+    }
+}
